ea4f9659af1cae61c844f037c9e9e5152c63cdec
Impally <Rodger.Friesen@gmail.com>
2015-08-04 22:18:51 -0400

    Start of shaders, better explanations of buffers given in this tutorial.

--- main.cpp
+++ main.cpp
@@ -2,34 +2,85 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+const char* vertex_shader=
+"#version 450\n"
+"in vec3 vp;"
+"void main(){"
+"gl_Position = vec4(vp, 1.0);"
+"}";
+
+const char* fragment_shader=
+"#version 450\n"
+"out vec4 frag_colour;"
+"void main(){"
+"frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+"}";
+
 int main(){
-	
-	if(!glfwInit())
-	{
-		fprintf(stderr, "ERROR: could not start GLFW3\n");
-		return 1;
-	}
-
+	//initializing everything
+	glfwInit();
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
-	if(!window)
-	{
-		fprintf(stderr, "ERROR: could not open window with GLFW\n");
-		glfwTerminate();
-		return 1;
-	}
-
 	glfwMakeContextCurrent(window);
 	glewExperimental = GL_TRUE;
 	glewInit();
-
-	const GLubyte* renderer = glGetString(GL_RENDERER);
-	const GLubyte* version = glGetString(GL_VERSION);
-	printf("Renderer: %s\n", renderer);
-	printf("OpenGL version supported %s\n", version);
-
 	//Tell GL to only draw onto a pixel if the shape is closer to the viewer
 	glEnable(GL_DEPTH_TEST); // enable depth-testing
 	glDepthFunc(GL_LESS);  //depth testing interprets a smaller value as closer
+
+	GLfloat points[] ={
+		0.0f, 0.5f, 0.0f,
+		0.5f,-0.5f, 0.0f,
+		-0.5f,-0.5f,0.0f
+	};
+	
+	//initializes Vertex Buffer Array
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//binds points into the buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	//initializing the Vertex Attribute Object
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	//Returns an unsigned integer to associate with this Vertex Array later
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	//Shader stuff
+	//associate Vertex Shader
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+
+	//associate and compile Fragment Shader
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	GLuint shader_programme = glCreateProgram();
+	glAttachShader(shader_programme, fs);
+	glAttachShader(shader_programme, vs);
+	glLinkProgram(shader_programme);
+
+	while(!glfwWindowShouldClose(window)){
+		//clear drawing surface
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(shader_programme);
+		glBindVertexArray(vao);
+		//draw the first three points in the currently bound VAO
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//update other things
+		glfwPollEvents();
+		//Swap the buffers into the window
+		glfwSwapBuffers(window);
+	}
+
+
+
 
 	glfwTerminate();
 	return 0;
